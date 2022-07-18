b0d542b36b38af10f6ae5ae368839c1d19ed0bd3
Mingshi Wang <mingshiwang123@gmail.com>
2022-07-17 22:03:34 -0500

    add telescope mapping

--- vim/.config/nvim/plugin/plugin-config/telescope-config.lua
+++ vim/.config/nvim/plugin/plugin-config/telescope-config.lua
@@ -1,6 +1,22 @@
 -- telescope remap
-vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope git_files<cr>', {noremap = true})
--- nnoremap <leader>ff
--- nnoremap <leader>fg <cmd>Telescope live_grep<cr>
--- nnoremap <leader>fb <cmd>Telescope buffers<cr>
--- nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope git_files<cr>',
+                        {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>ts',
+                        '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grep For > ")})<CR>',
+                        {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<Leader>tw',
+                        '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>")})<CR>',
+                        {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<Leader>tb',
+                        '<cmd>lua require("telescope.builtin").buffers()<CR>',
+                        {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<Leader>tq',
+                        '<cmd>lua require("telescope.builtin").quickfix()<CR>',
+                        {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<Leader>tr',
+                        '<cmd>lua require("telescope.builtin").registers()<CR>',
+                        {noremap = true})
