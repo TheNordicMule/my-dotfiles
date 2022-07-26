17c20c2bc25bbcc529dbd8e519af6501af23d5c2
Mingshi Wang <mingshiwang123@gmail.com>
2022-07-26 16:43:20 -0500

    use cmp

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -37,12 +37,14 @@ return require('packer').startup(function(use)
     -- lsp
     use 'neovim/nvim-lspconfig'
     use {"williamboman/mason.nvim"}
-    -- use 'hrsh7th/cmp-nvim-lsp'
-    -- use 'hrsh7th/cmp-buffer'
-    -- use 'hrsh7th/cmp-path'
-    -- use 'hrsh7th/cmp-cmdline'
-    -- use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/nvim-compe'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
+    use "rafamadriz/friendly-snippets"
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
     -- debugger
