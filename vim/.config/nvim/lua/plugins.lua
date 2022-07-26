f0afd111aa65bebcc59da1629cb9c4f56c89a456
Mingshi Wang <mingshiwang123@gmail.com>
2022-07-26 10:59:56 -0500

    regroup

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -13,34 +13,45 @@ end
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    -- fuzzy
     use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.0',
         requires = {{'nvim-lua/plenary.nvim'}}
     }
-    use 'vim-airline/vim-airline'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-fugitive'
-    use 'neovim/nvim-lspconfig'
 
-    -- all needed for completion
+    -- looks
+    use 'vim-airline/vim-airline'
+    use 'arcticicestudio/nord-vim'
+    use {
+        "startup-nvim/startup.nvim",
+        requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+        config = function() require"startup".setup({theme = "startify"}) end
+    }
+
+    -- git
+    use 'tpope/vim-fugitive'
+    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
+
+    -- lsp
+    use 'neovim/nvim-lspconfig'
+    use {"williamboman/mason.nvim"}
     -- use 'hrsh7th/cmp-nvim-lsp'
     -- use 'hrsh7th/cmp-buffer'
     -- use 'hrsh7th/cmp-path'
     -- use 'hrsh7th/cmp-cmdline'
     -- use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/nvim-compe'
-
-    use 'windwp/nvim-autopairs'
-    use 'arcticicestudio/nord-vim'
-    use 'iamcco/markdown-preview.nvim'
-    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
-    use 'puremourning/vimspector'
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+
+    -- debugger
+    use 'puremourning/vimspector'
     use 'szw/vim-maximizer'
-    use {
-        "startup-nvim/startup.nvim",
-        requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-        config = function() require"startup".setup({theme = "startify"}) end
-    }
+
+    -- utils
+    use 'tpope/vim-commentary'
+    use 'windwp/nvim-autopairs'
+    use 'iamcco/markdown-preview.nvim'
+
 end)
