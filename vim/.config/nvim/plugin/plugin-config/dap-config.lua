771773b980e3a8a3e64029a865efba05031fbc3f
Mingshi Wang <mingshiwang123@gmail.com>
2022-07-26 21:09:05 -0500

    [experimental] add dap debugger

--- vim/.config/nvim/plugin/plugin-config/dap-config.lua
+++ vim/.config/nvim/plugin/plugin-config/dap-config.lua
@@ -14,3 +14,57 @@ vim.api.nvim_set_keymap('n', "<Leader>dj", "<cmd>:lua require('dap').step_over()
 require("dapui").setup()
 require("nvim-dap-virtual-text").setup()
 vim.api.nvim_set_keymap('n', "<leader>m", "<cmd>:MaximizerToggle!<CR>", opts)
+
+local home = os.getenv('HOME')
+local dap = require('dap')
+dap.adapters.node2 = {
+    type = 'executable',
+    command = 'node',
+    args = {home .. '/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js'},
+}
+
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
+
+dap.configurations.typescript = {
+    {
+        name = "ts-node (Node2 with ts-node)",
+        type = "node2",
+        request = "launch",
+        cwd = vim.loop.cwd(),
+        runtimeArgs = { "-r", "ts-node/register" },
+        runtimeExecutable = "node",
+        args = {"--inspect", "${file}"},
+        sourceMaps = true,
+        skipFiles = { "<node_internals>/**", "node_modules/**" },
+    },
+    {
+        name = "Jest (Node2 with ts-node)",
+        type = "node2",
+        request = "launch",
+        cwd = vim.loop.cwd(),
+        runtimeArgs = {"--inspect-brk", "${workspaceFolder}/node_modules/.bin/jest"},
+        runtimeExecutable = "node",
+        args = {"${file}", "--runInBand", "--coverage", "false"},
+        sourceMaps = true,
+        port = 9229,
+        skipFiles = { "<node_internals>/**", "node_modules/**" },
+    },
+}
