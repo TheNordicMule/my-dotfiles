da59770a1b31e71e76587445620c4eb2fe5bb367
Mingshi Wang <mingshiwang123@gmail.com>
2022-08-01 14:20:50 -0500

    add oh my zsh and brew

--- clone_dependencies.sh
+++ clone_dependencies.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 (cd ..
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# nord theme tmux
 git clone https://github.com/arcticicestudio/nord-tmux.git
+# wall papers
 git clone https://github.com/linuxdotexe/nordic-wallpapers.git
+
+# oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 )
