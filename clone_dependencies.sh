a9de197322a65c5a5611ab685615fd311a6d7faf
Mingshi Wang <mingshiwang123@gmail.com>
2022-08-04 18:07:57 -0500

    update brewfile

--- clone_dependencies.sh
+++ clone_dependencies.sh
@@ -15,4 +15,6 @@ git clone https://github.com/linuxdotexe/nordic-wallpapers.git
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+brew bundle
 )
