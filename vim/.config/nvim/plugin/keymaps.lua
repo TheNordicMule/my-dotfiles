4db7c44a98eb9565d94e19d539c0e3a377fbdb59
Mingshi Wang <mingshiwang123@gmail.com>
2022-12-17 14:46:44 -0600

    add ctrl-u and d remap

--- vim/.config/nvim/plugin/keymaps.lua
+++ vim/.config/nvim/plugin/keymaps.lua
@@ -1,6 +1,6 @@
 -- Mappings.
-local silent_opts = { noremap=true, silent=true }
-local noremap = { noremap=true }
+local silent_opts = { noremap = true, silent = true }
+local noremap = { noremap = true }
 
 local buf_set_keymap = vim.api.nvim_set_keymap
 
@@ -16,69 +16,73 @@ buf_set_keymap('n', 'Q', '@@', noremap)
 -- bad habit. The former is enforceable through a .vimrc, while we don't know
 -- how to prevent the latter.
 -- Do this in normal mode...
-buf_set_keymap('n','<Left>','<cmd>echoe \'--use h--\'<CR>', noremap)
-buf_set_keymap('n','<Right>','<cmd>echoe \'--use l--\'<CR>', noremap)
-buf_set_keymap('n','<Up>','<cmd>echoe \'--use k--\'<CR>', noremap)
-buf_set_keymap('n','<Down>','<cmd>echoe \'--use j--\'<CR>', noremap)
+buf_set_keymap('n', '<Left>', '<cmd>echoe \'--use h--\'<CR>', noremap)
+buf_set_keymap('n', '<Right>', '<cmd>echoe \'--use l--\'<CR>', noremap)
+buf_set_keymap('n', '<Up>', '<cmd>echoe \'--use k--\'<CR>', noremap)
+buf_set_keymap('n', '<Down>', '<cmd>echoe \'--use j--\'<CR>', noremap)
 -- ...and in insert mode
-buf_set_keymap('i','<Left>','<cmd>echoe \'--use h--\'<CR>', noremap)
-buf_set_keymap('i','<Right>','<cmd>echoe \'--use l--\'<CR>', noremap)
-buf_set_keymap('i','<Up>','<cmd>echoe \'--use k--\'<CR>', noremap)
-buf_set_keymap('i','<Down>','<cmd>echoe \'--use j--\'<CR>', noremap)
+buf_set_keymap('i', '<Left>', '<cmd>echoe \'--use h--\'<CR>', noremap)
+buf_set_keymap('i', '<Right>', '<cmd>echoe \'--use l--\'<CR>', noremap)
+buf_set_keymap('i', '<Up>', '<cmd>echoe \'--use k--\'<CR>', noremap)
+buf_set_keymap('i', '<Down>', '<cmd>echoe \'--use j--\'<CR>', noremap)
 
 
-buf_set_keymap('n','<leader>ga','<cmd>Git add %<CR>', noremap)
-buf_set_keymap('n','<leader>gd','<cmd>Git diff<CR>', noremap)
-buf_set_keymap('n','<leader>gs','<cmd>Git status<CR>', noremap)
-buf_set_keymap('n','<leader>gb','<cmd>Git blame<CR>', noremap)
-buf_set_keymap('n','<leader>gc','<cmd>Git commit<CR>', noremap)
-buf_set_keymap('n','<leader>gr','<cmd>Git restore %<CR>', noremap)
-buf_set_keymap('n','<leader>gl','<cmd>Git log<CR>', noremap)
-buf_set_keymap('n','<leader>gv','<cmd>Gvdiffsplit!<CR>', noremap)
-buf_set_keymap('n','<leader>v','<Plug>MarkdownPreviewToggle', {})
+buf_set_keymap('n', '<leader>ga', '<cmd>Git add %<CR>', noremap)
+buf_set_keymap('n', '<leader>gd', '<cmd>Git diff<CR>', noremap)
+buf_set_keymap('n', '<leader>gs', '<cmd>Git status<CR>', noremap)
+buf_set_keymap('n', '<leader>gb', '<cmd>Git blame<CR>', noremap)
+buf_set_keymap('n', '<leader>gc', '<cmd>Git commit<CR>', noremap)
+buf_set_keymap('n', '<leader>gr', '<cmd>Git restore %<CR>', noremap)
+buf_set_keymap('n', '<leader>gl', '<cmd>Git log<CR>', noremap)
+buf_set_keymap('n', '<leader>gv', '<cmd>Gvdiffsplit!<CR>', noremap)
+buf_set_keymap('n', '<leader>v', '<Plug>MarkdownPreviewToggle', {})
 
 
 -- center search results
-buf_set_keymap('n','n','nzz',silent_opts)
-buf_set_keymap('n','N','Nzz',silent_opts)
-buf_set_keymap('n','*','*zz',silent_opts)
-buf_set_keymap('n','#','#zz',silent_opts)
-buf_set_keymap('n','g*','g*zz',silent_opts)
+buf_set_keymap('n', 'n', 'nzz', silent_opts)
+buf_set_keymap('n', 'N', 'Nzz', silent_opts)
+buf_set_keymap('n', '*', '*zz', silent_opts)
+buf_set_keymap('n', '#', '#zz', silent_opts)
+buf_set_keymap('n', 'g*', 'g*zz', silent_opts)
+
+-- center ctrl u and d
+buf_set_keymap('n', "<C-u>", "<C-u>zz", silent_opts)
+buf_set_keymap('n', "<C-d>", "<C-d>zz", silent_opts)
 
 -- use default very magic
-buf_set_keymap('n','?','?\\v', noremap)
-buf_set_keymap('n','/','/\\v', noremap)
-buf_set_keymap('c','%s/','%sm/', noremap)
+buf_set_keymap('n', '?', '?\\v', noremap)
+buf_set_keymap('n', '/', '/\\v', noremap)
+buf_set_keymap('c', '%s/', '%sm/', noremap)
 
 
 -- map <space>p and <space>y to copy to system clipboard
-buf_set_keymap('n','<leader>p','"+p', noremap)
-buf_set_keymap('v','<leader>p','"+p', noremap)
-buf_set_keymap('n','<leader>P','"+P', noremap)
-buf_set_keymap('v','<leader>P','"+P', noremap)
-buf_set_keymap('n','<leader>y','"+y', noremap)
-buf_set_keymap('v','<leader>y','"+y', noremap)
-buf_set_keymap('n','<leader>Y','"+y$', noremap)
-buf_set_keymap('n','<leader>d','"_d', noremap)
-buf_set_keymap('v','<leader>d','"_d', noremap)
+buf_set_keymap('n', '<leader>p', '"+p', noremap)
+buf_set_keymap('v', '<leader>p', '"+p', noremap)
+buf_set_keymap('n', '<leader>P', '"+P', noremap)
+buf_set_keymap('v', '<leader>P', '"+P', noremap)
+buf_set_keymap('n', '<leader>y', '"+y', noremap)
+buf_set_keymap('v', '<leader>y', '"+y', noremap)
+buf_set_keymap('n', '<leader>Y', '"+y$', noremap)
+buf_set_keymap('n', '<leader>d', '"_d', noremap)
+buf_set_keymap('v', '<leader>d', '"_d', noremap)
 
 -- Go to tab by number
-buf_set_keymap('n','<leader>t','<cmd>tabnew<cr>', noremap)
+buf_set_keymap('n', '<leader>t', '<cmd>tabnew<cr>', noremap)
 
 -- Quickfix list stuff
-buf_set_keymap('n','<leader>cn','<cmd>cn<CR>', noremap)
-buf_set_keymap('n','<leader>cp','<cmd>cp<CR>', noremap)
+buf_set_keymap('n', '<leader>cn', '<cmd>cn<CR>', noremap)
+buf_set_keymap('n', '<leader>cp', '<cmd>cp<CR>', noremap)
 
 -- quick move
-buf_set_keymap('v','J',':m \'>+1<CR>gv=gv', noremap)
-buf_set_keymap('v','K',':m \'<-2<CR>gv=gv', noremap)
+buf_set_keymap('v', 'J', ':m \'>+1<CR>gv=gv', noremap)
+buf_set_keymap('v', 'K', ':m \'<-2<CR>gv=gv', noremap)
 
 -- add resize
-buf_set_keymap('n','<leader>+',':vertical resize +5<CR>', noremap)
-buf_set_keymap('n','<leader>-',':vertical resize -5<CR>', noremap)
+buf_set_keymap('n', '<leader>+', ':vertical resize +5<CR>', noremap)
+buf_set_keymap('n', '<leader>-', ':vertical resize -5<CR>', noremap)
 
 -- swap j and gj, k and gk
-buf_set_keymap('n','j','gj', noremap)
-buf_set_keymap('n','k','gk', noremap)
-buf_set_keymap('n','gj','j', noremap)
-buf_set_keymap('n','gk','k', noremap)
+buf_set_keymap('n', 'j', 'gj', noremap)
+buf_set_keymap('n', 'k', 'gk', noremap)
+buf_set_keymap('n', 'gj', 'j', noremap)
+buf_set_keymap('n', 'gk', 'k', noremap)
