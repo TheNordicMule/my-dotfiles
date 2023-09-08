5e46b28789596e814ef6b704f939a275a83bfe0d
Mingshi Wang <mingshiwang123@gmail.com>
2023-09-07 21:54:54 -0500

    use lazy

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,67 +1,66 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({
-    'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-    install_path
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  execute 'packadd packer.nvim'
 end
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+vim.opt.rtp:prepend(lazypath)
 
-  -- fuzzy
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim', "debugloop/telescope-undo.nvim" } }
+return require("lazy").setup(
+  {
+    -- fuzzy
+    {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.2',
+      dependencies = { 'nvim-lua/plenary.nvim', "debugloop/telescope-undo.nvim" },
+    },
+
+    -- looks
+    'vim-airline/vim-airline',
+    'arcticicestudio/nord-vim',
+
+    -- git
+    'tpope/vim-fugitive',
+    { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+    -- lsp
+    'neovim/nvim-lspconfig',
+    { "williamboman/mason.nvim" },
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    "onsails/lspkind.nvim",
+
+    -- debugger
+    {
+      "rcarriga/nvim-dap-ui",
+      dependencies = { "mfussenegger/nvim-dap" }
+    },
+    'theHamsta/nvim-dap-virtual-text',
+    'szw/vim-maximizer',
+
+    -- utils
+    'tpope/vim-commentary',
+    'windwp/nvim-autopairs',
+    {
+      "ThePrimeagen/refactoring.nvim",
+      dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        { "nvim-treesitter/nvim-treesitter" }
+      }
+    },
+    'iamcco/markdown-preview.nvim'
   }
-
-  -- looks
-  use 'vim-airline/vim-airline'
-  use 'arcticicestudio/nord-vim'
-  use {
-    "startup-nvim/startup.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function() require "startup".setup({ theme = "startify" }) end
-  }
-
-  -- git
-  use 'tpope/vim-fugitive'
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-
-  -- lsp
-  use 'neovim/nvim-lspconfig'
-  use { "williamboman/mason.nvim" }
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
-  use "rafamadriz/friendly-snippets"
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use "onsails/lspkind.nvim"
-
-  -- debugger
-  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-  use 'theHamsta/nvim-dap-virtual-text'
-  use 'szw/vim-maximizer'
-
-  -- utils
-  use 'tpope/vim-commentary'
-  use 'windwp/nvim-autopairs'
-  use {
-    "ThePrimeagen/refactoring.nvim",
-    requires = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-treesitter/nvim-treesitter" }
-    }
-  }
-  use 'iamcco/markdown-preview.nvim'
-end)
+)
