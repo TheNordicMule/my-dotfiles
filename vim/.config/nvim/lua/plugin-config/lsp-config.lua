d64701d652cf54fee1d3bbaec7e3f28f144a9b8f
Mingshi Wang <mingshiwang123@gmail.com>
2023-11-28 18:09:12 -0600

    remove duplicate

--- vim/.config/nvim/lua/plugin-config/lsp-config.lua
+++ vim/.config/nvim/lua/plugin-config/lsp-config.lua
@@ -80,11 +80,3 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-local conform = require("conform")
-vim.keymap.set({ "n", "v" }, "<Leader>ff", function()
-  conform.format({
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 1000,
-  })
-end, {})
