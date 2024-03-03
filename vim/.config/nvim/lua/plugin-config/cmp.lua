79d91f9f9aa289e322534fc775077da491049aac
Mingshi Wang <mingshiwang123@gmail.com>
2024-03-03 11:35:52 -0600

    add cmdline supp

--- vim/.config/nvim/lua/plugin-config/cmp.lua
+++ vim/.config/nvim/lua/plugin-config/cmp.lua
@@ -50,7 +50,6 @@ cmp.setup({
     { name = 'luasnip' }, -- For luasnip users.
     { name = 'buffer' },
     { name = 'path' },
-    { name = 'cmdline' },
   }),
   formatting = {
     format = lspkind.cmp_format({
@@ -59,6 +58,23 @@ cmp.setup({
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
     })
   }
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
