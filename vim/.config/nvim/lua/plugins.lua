cd15aac6d1efb705b94432d7e6f244a78cff3b62
Mingshi Wang <mingshiwang123@gmail.com>
2024-03-10 17:43:24 -0500

    add lua source

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -74,6 +74,7 @@ return require("lazy").setup(
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
+        'hrsh7th/cmp-nvim-lua',
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
