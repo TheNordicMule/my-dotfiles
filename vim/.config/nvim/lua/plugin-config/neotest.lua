5f31329155d8a86f1b3738f500b2a3acb62c4f0b
Mingshi Wang <mingshiwang123@gmail.com>
2024-03-10 14:09:59 -0500

    use non trouble

--- vim/.config/nvim/lua/plugin-config/neotest.lua
+++ vim/.config/nvim/lua/plugin-config/neotest.lua
@@ -54,12 +54,6 @@ neotest.setup({
   status = { virtual_text = true },
   output = { open_on_run = true },
   quickfix = {
-    open = function()
-      if require("lazyvim.util").has("trouble.nvim") then
-        require("trouble").open({ mode = "quickfix", focus = false })
-      else
-        vim.cmd("copen")
-      end
-    end,
+    open = vim.cmd("copen")
   },
 })
