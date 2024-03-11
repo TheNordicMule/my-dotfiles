a843ce69fc68af8b5b9ca7ee36dc50b028d346f4
Mingshi Wang <mingshiwang123@gmail.com>
2024-03-11 13:48:03 -0500

    change keymap for harpoon

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -118,11 +118,11 @@ return require("lazy").setup(
             harpoon:setup()
             vim.keymap.set("n", "<leader>ma", function() harpoon:list():append() end)
             vim.keymap.set("n", "<leader>mm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-            vim.keymap.set("n", "<leader>m1", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<leader>m2", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<leader>m3", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<leader>m4", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader>m5", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
           end
     },
     {
