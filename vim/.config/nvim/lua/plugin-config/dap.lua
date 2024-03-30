def74dde196de2f38ed067e42d26102e3cb8c3d1
Mingshi Wang <mingshiwang123@gmail.com>
2024-03-30 15:25:01 -0500

    debug for rust

--- vim/.config/nvim/lua/plugin-config/dap.lua
+++ vim/.config/nvim/lua/plugin-config/dap.lua
@@ -66,3 +66,15 @@ dap.adapters.codelldb = {
     args = { "--port", "${port}" },
   }
 }
+dap.configurations.rust = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
