a0086951e565551c452c98332aae19d6a4203792
Mingshi Wang <mingshiwang123@gmail.com>
2024-04-11 23:04:59 -0500

    change harpoon mapping

--- vim/.config/nvim/lua/plugin-config/harpoon.lua
+++ vim/.config/nvim/lua/plugin-config/harpoon.lua
@@ -1,6 +1,6 @@
 local harpoon = require("harpoon")
 harpoon:setup()
-vim.keymap.set("n", "<leader>ma", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>ma", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>mm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
