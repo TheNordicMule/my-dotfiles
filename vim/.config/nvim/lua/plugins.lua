f6b1a292580a52b3942c3ba844ead5c36e80f2e6
Mingshi Wang <mingshiwang123@gmail.com>
2024-04-21 13:03:12 -0500

    add jsregexp to luasnip

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -80,7 +80,10 @@ return require("lazy").setup(
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-nvim-lua',
-        "L3MON4D3/LuaSnip",
+        {
+          "L3MON4D3/LuaSnip",
+          build = "make install_jsregexp"
+        },
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
         "onsails/lspkind.nvim",
