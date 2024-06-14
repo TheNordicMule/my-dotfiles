cb8c5561e6f0d6ec862136fae006bed797ac93ca
Mingshi Wang <mingshiwang123@gmail.com>
2024-06-14 17:37:10 -0500

    add install nix

--- clone_dependencies.sh
+++ clone_dependencies.sh
@@ -4,6 +4,9 @@
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# install nix
+curl -L https://nixos.org/nix/install
+
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
