1898429f7707fff2adfa54c6a0dbd650921ddb9e
Mingshi Wang <mingshiwang123@gmail.com>
2024-06-13 22:30:30 -0500

    28   2024-06-13 22:30:30   (current)

--- nix/nix-darwin/flake.nix
+++ nix/nix-darwin/flake.nix
@@ -24,6 +24,7 @@
       fonts.fonts = [
         pkgs.sketchybar-app-font
       ];
+      fonts.fontDir.enable = true;
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
