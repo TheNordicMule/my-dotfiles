ab04836bb979044dbd6f9b6554186cb157c3af4f
Mingshi Wang <mingshiwang123@gmail.com>
2024-06-15 20:17:38 -0500

    delete brewfiles and change clone step

--- clone_dependencies.sh
+++ clone_dependencies.sh
@@ -18,8 +18,4 @@ git clone https://github.com/linuxdotexe/nordic-wallpapers.git
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-brew bundle
-
-brew services start sketchybar
 )
