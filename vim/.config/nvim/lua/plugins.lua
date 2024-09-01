4ba91206f33e40a66c745af8222d721bd9b1ddad
Mingshi Wang <mingshiwang123@gmail.com>
2024-09-01 10:29:17 -0500

    change neodev

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -63,7 +63,19 @@ return require("lazy").setup({
 			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
-			"folke/neodev.nvim",
+			{
+				-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+				-- used for completion, annotations and signatures of Neovim apis
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{ "Bilal2453/luvit-meta", lazy = true },
 		},
 	},
 	{
@@ -83,6 +95,13 @@ return require("lazy").setup({
 			"rafamadriz/friendly-snippets",
 			"onsails/lspkind.nvim",
 		},
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
 	},
 
 	{
