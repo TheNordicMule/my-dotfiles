ea92ea0788bd88e2c367d291f0d57f328f6f2107
Mingshi Wang <mingshiwang123@gmail.com>
2024-10-05 14:28:33 -0500

    update config

--- reset_config.sh
+++ reset_config.sh
@@ -2,7 +2,7 @@
 # I stole this script from ThePrimeagen link here:
 # https://github.com/ThePrimeagen/.dotfiles/blob/master/install
 pushd ~/my-dotfiles
-folders=("alacritty" "amethyst" "sketchybar" "tmux" "vim" "zsh")
+folders=("aerospace" "wezterm" "sketchybar" "tmux" "vim" "zsh")
 for folder in "${folders[@]}"
 do
     stow -D $folder
