3d0f8a116e422b49989f0fa6b56f31bf78824d22
Mingshi Wang <mingshiwang123@gmail.com>
2025-03-05 22:48:09 -0600

    delete luarc

--- vim/.config/nvim/lua/plugin-config/lsp.lua
+++ vim/.config/nvim/lua/plugin-config/lsp.lua
@@ -32,7 +32,7 @@ require("mason").setup({
 	PATH = "append",
 })
 
-local ensure_installed = { "pyright", "clangd", "gopls", "rust_analyzer", "lua_ls", "nil", "ocamllsp" }
+local ensure_installed = { "pyright", "clangd", "gopls", "rust_analyzer", "nil", "ocamllsp", "lua_ls" }
 
 vim.list_extend(ensure_installed, {
 	"stylua", -- Used to format Lua code
@@ -48,40 +48,6 @@ require("mason-lspconfig").setup({
 		function(server_name) -- default handler (optional)
 			require("lspconfig")[server_name].setup({
 				capabilities = capabilities,
-			})
-		end,
-
-		-- lua ls setup
-		["lua_ls"] = function(server_name)
-			require("lspconfig")[server_name].setup({
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						diagnostics = {
-							-- Get the language server to recognize the `vim` global
-							globals = {
-								"vim",
-								"require",
-							},
-						},
-						workspace = {
-							checkThirdParty = false,
-							-- Tells lua_ls where to find all the Lua files that you have loaded
-							-- for your neovim configuration.
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-							-- If lua_ls is really slow on your computer, you can try this instead:
-							-- library = { vim.env.VIMRUNTIME },
-						},
-						completion = {
-							callSnippet = "Replace",
-						},
-						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-						-- diagnostics = { disable = { 'missing-fields' } },
-					},
-				},
 			})
 		end,
 	},
--- vim/.config/nvim/lua/plugin-config/telescope.lua
+++ vim/.config/nvim/lua/plugin-config/telescope.lua
@@ -1,31 +1,49 @@
 -- telescope remap
 vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<cr>")
-vim.keymap.set("n", "<Leader>tp", '<cmd>lua require("telescope.builtin").find_files({ hidden=true })<CR>')
-vim.keymap.set(
-	"n",
-	"<Leader>ts",
-	'<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grep For > "), use_regex=true})<CR>'
-)
+vim.keymap.set("n", "<Leader>tp", function()
+	require("telescope.builtin").find_files({ hidden = true })
+end)
 
-vim.keymap.set(
-	"n",
-	"<Leader>tw",
-	'<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>")})<CR>'
-)
+vim.keymap.set("n", "<Leader>ts", function()
+	require("telescope.builtin").grep_string({
+		search = vim.fn.input("Grep For > "),
+		use_regex = true,
+	})
+end)
 
-vim.keymap.set("n", "<Leader>tb", '<cmd>lua require("telescope.builtin").buffers()<CR>')
+vim.keymap.set("n", "<Leader>tw", function()
+	require("telescope.builtin").grep_string({
+		search = vim.fn.expand("<cword>"),
+	})
+end)
 
-vim.keymap.set("n", "<Leader>tq", '<cmd>lua require("telescope.builtin").quickfix()<CR>')
+vim.keymap.set("n", "<Leader>tb", function()
+	require("telescope.builtin").buffers()
+end)
 
-vim.keymap.set("n", "<Leader>tr", '<cmd>lua require("telescope.builtin").registers()<CR>')
+vim.keymap.set("n", "<Leader>tq", function()
+	require("telescope.builtin").quickfix()
+end)
 
-vim.keymap.set("n", "<Leader>td", '<cmd>lua require("telescope.builtin").diagnostics()<CR>')
+vim.keymap.set("n", "<Leader>tr", function()
+	require("telescope.builtin").registers()
+end)
 
-vim.keymap.set("n", "<Leader>th", '<cmd>lua require("telescope.builtin").help_tags()<CR>')
+vim.keymap.set("n", "<Leader>td", function()
+	require("telescope.builtin").diagnostics()
+end)
 
-vim.keymap.set("n", "<Leader>tc", '<cmd>lua require("telescope.builtin").commands()<CR>')
+vim.keymap.set("n", "<Leader>th", function()
+	require("telescope.builtin").help_tags()
+end)
 
-vim.keymap.set("n", "<Leader>tg", '<cmd>lua require("telescope.builtin").git_branches()<CR>')
+vim.keymap.set("n", "<Leader>tc", function()
+	require("telescope.builtin").commands()
+end)
+
+vim.keymap.set("n", "<Leader>tg", function()
+	require("telescope.builtin").git_branches()
+end)
 
 local actions = require("telescope.actions")
 
