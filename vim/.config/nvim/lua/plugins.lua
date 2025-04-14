0b3bfc7cecb3283cc7fbc39423cac9e4344c8d34
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-14 14:44:44 -0500

    add back lazy dev

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -97,8 +97,20 @@ return require("lazy").setup({
 				},
 				opts = {},
 			},
-			"folke/lazydev.nvim",
-      "onsails/lspkind.nvim"
+			{
+				-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+				-- used for completion, annotations and signatures of Neovim apis
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{ "Bilal2453/luvit-meta", lazy = true },
+			"onsails/lspkind.nvim",
 		},
 	},
 
