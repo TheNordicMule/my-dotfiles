2043ef49044036fd7f95ddf5468c4c30d852b11f
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-14 21:16:57 -0500

    add blink

--- vim/.config/nvim/lua/plugins/blink.lua
+++ vim/.config/nvim/lua/plugins/blink.lua
@@ -34,5 +34,74 @@ return {
 			{ "Bilal2453/luvit-meta", lazy = true },
 			"onsails/lspkind.nvim",
 		},
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<C-l>"] = { "snippet_forward", "fallback" },
+				["<C-h>"] = { "snippet_backward", "fallback" },
+			},
+			appearance = {
+				nerd_font_variant = "normal",
+			},
+
+			completion = {
+				-- By default, you may press `<c-space>` to show the documentation.
+				-- Optionally, set `auto_show = true` to show the documentation after a delay.
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+				menu = {
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = require("lspkind").symbolic(ctx.kind, {
+											mode = "symbol",
+										})
+									end
+
+									return icon .. ctx.icon_gap
+								end,
+
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+						},
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+					},
+					min_width = 30,
+				},
+			},
+
+			sources = {
+				default = { "lsp", "path", "snippets", "lazydev" },
+				providers = {
+					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+				},
+			},
+
+			snippets = { preset = "luasnip" },
+
+			fuzzy = { implementation = "lua" },
+
+			signature = { enabled = true },
+		},
 	},
 }
