603e2cb654e90a188b0fed32065f388e93a52257
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-14 23:36:00 -0500

    make git signs work

--- vim/.config/nvim/lua/plugins/git.lua
+++ vim/.config/nvim/lua/plugins/git.lua
@@ -1,58 +1,63 @@
 return {
-	"tpope/vim-fugitive",
-	dependencies = { "tpope/vim-rhubarb" },
-	{ "lewis6991/gitsigns.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+	{
+		"tpope/vim-fugitive",
+		dependencies = { "tpope/vim-rhubarb" },
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gs.nav_hunk("next")
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gs.nav_hunk("prev")
+					end
+				end)
+				-- Actions
+				map("n", "<leader>ha", gs.stage_hunk)
+				map("n", "<leader>hr", gs.reset_hunk)
+				map("v", "<leader>ha", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+				map("v", "<leader>hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+				map("n", "<leader>hS", gs.stage_buffer)
+				map("n", "<leader>hu", gs.undo_stage_hunk)
+				map("n", "<leader>hR", gs.reset_buffer)
+				map("n", "<leader>hp", gs.preview_hunk)
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = true })
+				end)
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+			end,
 		},
-		on_attach = function(bufnr)
-			local gs = package.loaded.gitsigns
-
-			local function map(mode, l, r, opts)
-				opts = opts or {}
-				opts.buffer = bufnr
-				vim.keymap.set(mode, l, r, opts)
-			end
-
-			-- Navigation
-			map("n", "]c", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "]c", bang = true })
-				else
-					gs.nav_hunk("next")
-				end
-			end)
-
-			map("n", "[c", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "[c", bang = true })
-				else
-					gs.nav_hunk("prev")
-				end
-			end)
-			-- Actions
-			map("n", "<leader>ha", gs.stage_hunk)
-			map("n", "<leader>hr", gs.reset_hunk)
-			map("v", "<leader>ha", function()
-				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end)
-			map("v", "<leader>hr", function()
-				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end)
-			map("n", "<leader>hS", gs.stage_buffer)
-			map("n", "<leader>hu", gs.undo_stage_hunk)
-			map("n", "<leader>hR", gs.reset_buffer)
-			map("n", "<leader>hp", gs.preview_hunk)
-			map("n", "<leader>hb", function()
-				gs.blame_line({ full = true })
-			end)
-			-- Text object
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-		end,
 	},
 }
