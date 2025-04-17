d135514f2232410632fc3e853b62905fe293d13f
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-16 21:38:48 -0500

    add starship

--- reset_config.sh
+++ reset_config.sh
@@ -2,7 +2,7 @@
 # I stole this script from ThePrimeagen link here:
 # https://github.com/ThePrimeagen/.dotfiles/blob/master/install
 pushd ~/my-dotfiles
-folders=("aerospace" "wezterm" "sketchybar" "tmux" "vim" "zsh")
+folders=("aerospace" "wezterm" "sketchybar" "tmux" "vim" "zsh", "starship")
 for folder in "${folders[@]}"
 do
     stow -D $folder
