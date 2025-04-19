26a01da0cd324c0f7b6db358abbee4381b49bc78
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-19 17:22:24 -0500

    add nord config back

--- config/nvim/lua/plugins/looks.lua
+++ config/nvim/lua/plugins/looks.lua
@@ -19,10 +19,23 @@ return {
 				flavour = "mocha",
 				transparent_background = true,
 			})
-      vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
-	"nordtheme/vim",
+	{
+		"nordtheme/vim",
+		--   priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("nord")
+		--
+		-- 	-- Remove background color from Normal and other UI groups
+		-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+		-- end,
+	},
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
