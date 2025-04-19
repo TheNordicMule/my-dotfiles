1819cca6691e637b91f02b6355e9c5c7c5b1a26e
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-18 19:14:02 -0500

    use official nord

--- vim/.config/nvim/lua/plugins/looks.lua
+++ vim/.config/nvim/lua/plugins/looks.lua
@@ -8,13 +8,17 @@ return {
 		},
 	},
 	{
-		"gbprod/nord.nvim",
+		"nordtheme/vim",
 		priority = 1000,
 		config = function()
-			require("nord").setup({
-				transparent = true,
-			})
 			vim.cmd.colorscheme("nord")
+
+			-- Remove background color from Normal and other UI groups
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+			vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
 		end,
 	},
 	{
