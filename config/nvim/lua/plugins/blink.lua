8a3dfa271a35306197596b315c587c336c88d5fe
Mingshi Wang <mingshiwang123@gmail.com>
2025-04-29 11:10:41 -0500

    add blink.compat

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -2,16 +2,16 @@ return { -- Autocompletion
 	"saghen/blink.cmp",
 	event = "InsertEnter",
 	version = "1.*",
-	{
-		"saghen/blink.compat",
-		-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-		version = "*",
-		-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-		lazy = true,
-		-- make sure to set opts so that lazy.nvim calls blink.compat's setup
-		opts = {},
-	},
 	dependencies = {
+		{
+			"saghen/blink.compat",
+			-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+			version = "*",
+			-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+			lazy = true,
+			-- make sure to set opts so that lazy.nvim calls blink.compat's setup
+			opts = {},
+		},
 		"rcarriga/cmp-dap",
 		{ "nvim-tree/nvim-web-devicons", opts = {} },
 		-- Snippet Engine
