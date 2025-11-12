273596134d26e10cae8a323f33124512302e2caa
Mingshi Wang <mingshiwang123@gmail.com>
2025-11-11 23:44:01 -0600

    add diagnostic keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -85,3 +85,17 @@ buf_set_keymap(
 	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
 	{ desc = "Redraw / Clear hlsearch / Diff Update" }
 )
+
+-- Diagnostic
+buf_set_keymap("n", "]e", function()
+	vim.diagnostic.jump({ severity = vim.diagnostic.severity.ERROR, count = 1 })
+end, { desc = "Next Error" })
+buf_set_keymap("n", "[e", function()
+	vim.diagnostic.jump({ severity = vim.diagnostic.severity.ERROR, count = -1 })
+end, { desc = "Prev Error" })
+buf_set_keymap("n", "]w", function()
+	vim.diagnostic.jump({ severity = vim.diagnostic.severity.WARN, count = 1 })
+end, { desc = "Next Warning" })
+buf_set_keymap("n", "[w", function()
+	vim.diagnostic.jump({ severity = vim.diagnostic.severity.WARN, count = -1 })
+end, { desc = "Prev Warning" })
