9e18e67f50f51afbb919c05daebe3c82ad3cf6e2
Mingshi Wang <mingshiwang123@gmail.com>
2026-04-03 17:01:04 -0500

    Revert "get rid of treesitter unnucessary config"
    This reverts commit 0c034892e31c633c9d48d001ff875f1ad41dcacb.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -7,4 +7,12 @@ return {
 		{ "<leader>ui", "<cmd>Inspect<CR>", desc = "Inspect Node" },
 		{ "<leader>uI", "<cmd>InspectTree<CR>", desc = "Inspect Tree" },
 	},
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter-highlight", { clear = true }),
+			callback = function(ev)
+				pcall(vim.treesitter.start, ev.buf)
+			end,
+		})
+	end,
 }
