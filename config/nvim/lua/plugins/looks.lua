3dfa709bfae116c9d98277c6941b2ff0658f91ee
Mingshi Wang <mingshiwang123@gmail.com>
2026-07-05 20:29:06 -0500

    use nord

--- config/nvim/lua/plugins/looks.lua
+++ config/nvim/lua/plugins/looks.lua
@@ -11,25 +11,22 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-				snacks = {
-					enabled = true,
-				},
-				float = {
-					transparent = true, -- enable transparent floating windows
-				},
-			})
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
+		-- priority = 1000,
+		-- config = function()
+		-- 	require("catppuccin").setup({
+		-- 		flavour = "mocha",
+		-- 		transparent_background = true,
+		-- 		float = {
+		-- 			transparent = true,
+		-- 			solid = false,
+		-- 		},
+		-- 	})
+		-- 	vim.cmd.colorscheme("catppuccin-mocha")
+		-- end,
 	},
 	{
 		"gbprod/nord.nvim",
 		priority = 1000,
-		enabled = false,
 		config = function()
 			require("nord").setup({})
 			vim.cmd.colorscheme("nord")
