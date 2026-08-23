b9a2e1ed7d6d0ad49f9ce693601b55124ed5e583
Mingshi Wang <mingshiwang123@gmail.com>
2026-08-23 16:44:24 -0500

    try to use native markdown

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -1,14 +1,9 @@
 return {
-	-- install without yarn or npm
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		keys = {
-			{ "<leader>v", "<Plug>MarkdownPreviewToggle", desc = "Markdown toggle" },
-		},
-	},
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
 }
